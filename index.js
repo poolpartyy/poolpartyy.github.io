543b0e2f09bb26b7f67b804e6edafa74a11e0e5e
Jérémie Lagache <jeremielagache3@gmail.com>
2022-06-22 20:48:01 +0200

    pool party site

--- index.js
+++ index.js
@@ -9,11 +9,14 @@ button.addEventListener("click", () => {
     localStorage.setItem("heure1", document.querySelector("#heure1").value);
     localStorage.setItem("heure2", document.querySelector("#heure2").value);
     
+    
     if (document.querySelector("#nom").value !== "" && document.querySelector("#adresse").value !== "" && document.querySelector("#capacite").value !== "" && document.querySelector("#phone").value !== "" && document.querySelector("#heure1").value !== "" && document.querySelector("#heure2").value !== "") {
  
         
+        console.log("")
         
     }
+
 });
 
 
---
+++ parties.js
@@ -0,0 +1,10 @@
+let nom = localStorage.getItem("nom");
+let oldNom = nom;
+
+let running = true;
+
+while (running = true) {
+    if (oldNom != nom) {
+        alert("done")
+    }
+}
