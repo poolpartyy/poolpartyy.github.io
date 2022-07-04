8cb1e8def0b2cb793bee667d11a7d454be03cb96
Jérémie Lagache <jeremielagache3@gmail.com>
2022-07-04 14:06:52 +0200

    pool party site php

--- formulairePOST.php
+++ formulairePOST.php
@@ -22,7 +22,7 @@ $heure1 = "";
 $heure2 = "";
 $number = 0;
 
-if(isset($_GET['button'])) {
+if(isset($_POST['button'])) {
     try{
         $conn = new PDO("mysql:host=$servname;dbname=$dbname", $user, $pass);
         $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
@@ -35,12 +35,12 @@ if(isset($_GET['button'])) {
 }
 
 if ($number === 1) {
-    $date = $_GET["jour"];
-    $adresse = $_GET["adresse"];
-    $capacite = $_GET["capacite"];
-    $phone = $_GET["phone"];
-    $heure1 = $_GET["heure1"];
-    $heure2 = $_GET["heure2"];
+    $date = $_POST["jour"];
+    $adresse = $_POST["adresse"];
+    $capacite = $_POST["capacite"];
+    $phone = $_POST["phone"];
+    $heure1 = $_POST["heure1"];
+    $heure2 = $_POST["heure2"];
 
     $sql = "INSERT INTO tables(date, heure1, heure2, adresse, capacite, phone)VALUES('.$date', '.$heure1', '.$heure2', '.$adresse', '.$capacite', '.$phone')";
 
